32bcb4f896db89dd69119e38662c7c0e7d11851e
Hilmi Thabibi <99851501+WhiteHilmi@users.noreply.github.com>
2025-02-12 10:24:02 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -150,7 +150,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://http://ecommerce.bbhealthyshop.com/'
+    baseURL: 'http://ecommerce.bbhealthyshop.com/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
